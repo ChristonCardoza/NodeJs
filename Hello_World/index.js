a02947fea5a69329ead582e0e1a3eda1e4de2cbc
Christon Cardoza <Christon.Antony_cardoza@rsa.com>
2020-08-27 16:29:21 +0530

    Hello World using NodeJs is added

---
+++ Hello_World/index.js
@@ -0,0 +1,39 @@
+console.log("***************************************************************************");
+console.log("*                                Hello World                              *");
+console.log("***************************************************************************");
+console.log("Hello World!!!!\n");
+
+
+
+console.log("***************************************************************************");
+console.log("*                             Create New Module                           *");
+console.log("***************************************************************************");
+console.log("Create a new node module/poject, which contain 'package.json' which has details of module, by running:\n\t'npm init'");
+
+
+
+console.log("***************************************************************************");
+console.log("*                             Run The Module                              *");
+console.log("***************************************************************************");
+console.log("For running the node application:\n\t 1. Add your entrypoint javascript in package.json ie \n\t\t \"scripts\":\n\t\t \{\n\t\t\t\"start\": \"node index.js\"\n\t\t\}\n\t 2. In command prompt run :\n\t\t\t npm start\n");
+
+
+
+console.log("***************************************************************************");
+console.log("*                             Run The Custom                              *");
+console.log("***************************************************************************");
+console.log("We can also have other than start and test custom mapping inside script in package.json:\n\t 1. Add your custom mapping data/javascript in package.json ie \n\t \"scripts\":\n\t\t \{\n\t\t\t\"<custom_key>\": \"<custom data/javascript_file_name>\"\n\t\t\}\n\t 2. In command prompt run : npm run <custom_key>\n");
+
+
+
+console.log("***************************************************************************");
+console.log("*                          Package Installation                           *");
+console.log("***************************************************************************");
+console.log("Installing new node packages rom npm:\n 'npm install <package_name>'\n\t This will create new 2 things in your current module:\n\t\t 1. Object named'dependencies' is added in your current 'package.json'\n\t\t 2. New folder named node_modules is created contains new packages");
+
+
+
+console.log("***************************************************************************");
+console.log("*                        Dependencies Installation                        *");
+console.log("***************************************************************************");
+console.log("To install all  the required modules mentioned in 'dependencies' object in 'package.json'\n\t 'npm install'");
