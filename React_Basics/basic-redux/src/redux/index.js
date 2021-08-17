59b117332f40ee6c5d749f0c55639fc151b6819c
Christon Cardoza <christon.antony_cardoza@rsa.com>
2021-08-18 00:50:15 +0530

    Removed React practice

--- React_Basics/basic-redux/src/redux/index.js
+++
@@ -1,3 +0,0 @@
-export { buyCake } from './cakes/cakeAction'
-export { buyIceCream } from './iceCream/iceCreamAction'
-export { fetchUsersRequest, fetchUsersSuccess, fetchUsersFailure } from './user/userAction'
